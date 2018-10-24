f410b37cd4eeec826e97546c8060c8b184fcd8f6
Thomas Kowalski <thom.kowa@gmail.com>
2018-10-24 12:27:15 +0200

    (Projet) Avancement sur les graphes

---
+++ Projet/Graph.ml
@@ -0,0 +1,38 @@
+module Graph = sig
+    type node
+    (* module S : Map.S with type key = node *)
+    type graph 
+
+    val distance : node -> node -> graph -> float
+    val distance_path : node list -> graph -> float
+    val empty : graph
+end
+
+module MakeCompleteGraph : Graph = struct
+    module Node = struct
+        type t = int
+        let compare x y = x - y
+    end 
+    type node = Node.t
+    type pair = string * (float * float)
+
+    module IntMap = Map.Make(Node)
+    type graph = pair IntMap.t
+
+    let distance u v g =
+        let data_u = IntMap.find u g in
+        let data_v = IntMap.find v g in 
+        match data_u, data_v with
+        | (_, (xu, yu)), (_, (xv, yv)) -> ((xu -. xv) *. (xu -. xv) +. (yu -. yv) *. (yu -. yv)) 
+
+    let rec distance_path path g = match path with
+    | [] -> 0.
+    | h::t -> let data = IntMap.find h g in
+        match data with
+        | _, (x, y) -> x +. y +. (distance_path t g)
+
+    let empty = IntMap.empty
+
+    let add_node index name x y g = 
+        IntMap.add index (name, (x, y)) g 
+end
---
+++ Projet/tests_complete_graph.ml
@@ -0,0 +1,5 @@
+open Graph
+
+let g = Graph.MakeCompleteGraph
+
+let test = g.add 0 ("Paris", (1.0, 0.0))
